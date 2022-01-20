6ed590b7b9efd42286a6be06ced34b540f06acba
Marco Sandri <sandrimarco98@gmail.com>
2022-01-20 22:50:20 +0100

    Aggiunto un layout grafico

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ let discountedPrice;
 
 // Controllo dati inseriti
 if (isNaN(parseInt(eta)) || isNaN(parseInt(distance))) {
-    document.getElementById('error').innerHTML = "Inserisci dei dati validi";
+    document.getElementById('error').innerHTML = "INSERIRE DATI VALIDI";
 } else {
     // Calcolo sconto
     if (eta < 18) {
         discountedPrice = price * 80 / 100;
+        document.getElementById('oldprice').innerHTML = price + "€";
     } else if (eta > 65) {
         discountedPrice = price * 60 / 100;
+        document.getElementById('oldprice').innerHTML = price + "€";
     } else {
         discountedPrice = price;
     }
@@ -24,3 +26,15 @@ if (isNaN(parseInt(eta)) || isNaN(parseInt(distance))) {
 
 // Restituisce sconto a schermo
 document.getElementById('discounted').innerHTML = discountedPrice.toFixed(2);
+document.getElementById('age').innerHTML = eta;
+document.getElementById('discounted').innerHTML = discountedPrice.toFixed(2) + "€";
+
+// Generatore random di numero di sedile e numero del treno
+let seat = Math.floor(Math.random() * 100) + "A";
+let number = Math.floor(Math.random() * 1000000);
+
+document.getElementById('seat').innerHTML = seat;
+document.getElementById('number').innerHTML = number;
+
+document.getElementById('seat-cutting').innerHTML = seat;
+document.getElementById('number-cutting').innerHTML = number;
