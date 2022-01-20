676131669ed7a9af9ae28a78d93a6800968ffe14
Marco Sandri <sandrimarco98@gmail.com>
2022-01-20 18:23:57 +0100

    Controllo variabili

--- main.js
+++ main.js
@@ -2,19 +2,24 @@
 let eta = prompt("Inserisca la sua età");
 let distance = prompt("Inserisca i kilometri che deve fare");
 
+
 // Prezzi
 let price = distance * 0.21;
 let discountedPrice;
 
 
-// Calcolo sconto
-if (eta < 18) {
-    discountedPrice = price * 80 / 100;
-
-} else if (eta > 65) {
-    discountedPrice = price * 60 / 100;
+// Controllo dati inseriti
+if (isNaN(parseInt(eta)) || isNaN(parseInt(distance))) {
+    document.getElementById('error').innerHTML = "Inserisci dei dati validi";
 } else {
-    discountedPrice = price;
+    // Calcolo sconto
+    if (eta < 18) {
+        discountedPrice = price * 80 / 100;
+    } else if (eta > 65) {
+        discountedPrice = price * 60 / 100;
+    } else {
+        discountedPrice = price;
+    }
 }
 
 // Restituisce sconto a schermo
