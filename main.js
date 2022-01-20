a294e52ded596065bf263761ba7773fb61fb0da5
Marco Sandri <sandrimarco98@gmail.com>
2022-01-20 18:20:34 +0100

    Commento

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 let eta = prompt("Inserisca la sua età");
 let distance = prompt("Inserisca i kilometri che deve fare");
 
-
+// Prezzi
 let price = distance * 0.21;
 let discountedPrice;
 
+
+// Calcolo sconto
 if (eta < 18) {
     discountedPrice = price * 80 / 100;
 
@@ -15,7 +17,5 @@ if (eta < 18) {
     discountedPrice = price;
 }
 
-
-
-
+// Restituisce sconto a schermo
 document.getElementById('discounted').innerHTML = discountedPrice.toFixed(2);
